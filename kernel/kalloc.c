13dc814f5c1dea2e1b23a06e405839078a8da84c
hzx-coder0 <2031294950@qq.com>
2025-12-27 17:06:59 +0800

    完成 Memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -19,15 +19,16 @@ struct run {
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
-} kmem;
+    struct spinlock lock;
+    struct run *freelist;
+} kmem[NPROC]; // 每个 CPU 都拥有自个儿的可用内存链表
 
 void
-kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+kinit() {      
+//  initlock(&kmem.lock, "kmem");
+    for (int i = 0; i < NPROC; i++)
+        initlock(&kmem[i].lock, "kmem"); // 初始化每个 CPU 的锁
+    freerange(end, (void *) PHYSTOP);
 }
 
 void
@@ -44,39 +45,72 @@ freerange(void *pa_start, void *pa_end)
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void
-kfree(void *pa)
-{
+kfree(void *pa) {
+          
+          
+    
   struct run *r;
+  int id = cpuid();
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+  if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP)
+      panic("kfree");
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *) pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // 原来的逻辑，加锁，然后把空闲页加到链表的头部，释放锁，搞定
+  //  acquire(&kmem.lock);
+  //  r->next = kmem.freelist;
+  //  kmem.freelist = r;
+  //  release(&kmem.lock);
+
+  // 改为，把空闲页加入到正在执行的 CPU 的链表中
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  //printf("cpu %d free 1 page\n", id);
+  release(&kmem[id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *
-kalloc(void)
-{
-  struct run *r;
+kalloc(void) {
+            
+            
+      
+    struct run *r = 0;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    // 原来的逻辑，加锁，然后从链表头取一个空闲页，释放锁
+    //  acquire(&kmem.lock);
+    //  r = kmem.freelist;
+    //  if(r)
+    //    kmem.freelist = r->next;
+    //  release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    // 改为，优先从当前 CPU（curid）中获取一个空闲页，如果失败，从当前 CPU 右手边开始遍历，直到找到一页或者都没有可用内存
+    for (int i = 0, curid = cpuid(); i < NPROC && !r; i++, curid++) {
+            
+            
+      
+        if (curid == NPROC)
+            curid = 0; // 环形遍历
+        acquire(&kmem[curid].lock);
+        r = kmem[curid].freelist;
+        if (r) {
+            
+            
+      
+            kmem[curid].freelist = r->next;
+            //printf("cpu %d steal 1 page from cpu %d\n", cpuid(), curid);
+        }
+        release(&kmem[curid].lock);
+    }
+
+    if (r)
+        memset((char *) r, 5, PGSIZE); // fill with junk
+    return (void *) r;
 }
